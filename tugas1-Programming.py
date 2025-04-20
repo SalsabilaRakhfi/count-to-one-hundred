0a073dcfc6473eab03ae32717cf53c7a7cf46a58
SalsabilaRakhfi <salsabila.rakhfi@ac.ui.id>
2025-04-20 20:00:59 +0700

    Add tugas1-Programming.py

--- tugas1-Programming.py
+++ tugas1-Programming.py
@@ -1,10 +1,12 @@
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 
-# ubah age ke tipe angka
-age = int(age)
+year_to_100 = 100 - age
 
 # hitung berapa tahun lagi menuju umur 100 (walau ga mau si)
 years_to_100 = 100 - age
 
-print(f"{name} will be 100 years old in {years_to_100} years")
+current_year = 2025 
+year_when_100 = current_year + year_to_100
+
+print(f"{name} will be 100 years old in {year_when_100}")
